27738b89a192b7e07176f84978099f3e3ae3f7e7
luke-chant <luke.chant@autotrader.co.uk>
2023-01-23 11:48:49 +0000

    Initial set up

---
+++ src/app/components/parent/parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+
+export class ParentComponent {
+  userName = 'Maria';
+
+  updateUser() {
+     this.userName = 'Chris';
+  }
+}
