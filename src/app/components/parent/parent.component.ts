cdba16f8c9899e0bd91e64ddde15a8b4aff7e484
luke-chant <luke.chant@autotrader.co.uk>
2023-01-30 09:16:28 +0000

    text changes, add some spacing

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent implements OnInit {
-  userName = 'Maria';
+  userName = 'Luke';
   isChildDestroyed = false;
 
   ngOnInit() {
